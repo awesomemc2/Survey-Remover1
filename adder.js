83624fabd77eeec39f39df0572928462f944fa4d
awesomemc2 <edisondieu22@gmail.com>
2016-06-13 19:57:50 -0700

    Create adder.js
    bookmark this

---
+++ adder.js
@@ -0,0 +1,13 @@
+// this is a adder book mark it
+// project by:me
+
+var (1adder)
+
+if the servey it there
+ if it started
+  survey.remover.server(111BED)
+end
+
+if the servey are there
+ quick.remover("10x")
+end
